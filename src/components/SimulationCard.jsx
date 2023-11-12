b278673df7d94d1b2cf91a894aec6dac95fb5b1c
tehbotolbayu <bayuuabdur2903@gmail.com>
2023-11-12 14:16:17 +0800

    v2

---
+++ src/components/SimulationCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SimulationCard = () => {
+  return (
+    <>
+      <div className="card w-96 h-96">
+
+      </div>
+    </>
+  )
+}
+
+export default SimulationCard
